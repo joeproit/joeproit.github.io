cab5d6109f3ecad3af3a366ef58a3dac41d8325c
JoePro <git@joeproit.com>
2023-06-09 11:51:35 -0500

    just 4

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-fetch('https://api.github.com/users/joeproit/repos')
+fetch('https://api.github.com/users/username/repos')
   .then(response => response.json())
   .then(data => {
     // Sort repositories by last updated date (descending order)
     const sortedRepos = data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+
+    // Get the last 4 updated repositories
+    const last4Repos = sortedRepos.slice(0, 4);
 
     // Generate the HTML for repository cards
     const repoListElement = document.getElementById('repoList');
     const grid = document.createElement('div');
     grid.className = 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8';
 
-    sortedRepos.forEach(repo => {
+    last4Repos.forEach(repo => {
       // Create repository card elements
       const card = document.createElement('div');
       card.className = 'course-card';
