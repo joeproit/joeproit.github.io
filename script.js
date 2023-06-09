7bf4e9c996ce8998a5d01640a382b283dd869715
JoePro <git@joeproit.com>
2023-06-09 11:48:32 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+fetch('https://api.github.com/users/username/repos')
+  .then(response => response.json())
+  .then(data => {
+    // Sort repositories by last updated date (descending order)
+    const sortedRepos = data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+
+    // Generate the HTML for repository cards
+    const repoListElement = document.getElementById('repoList');
+    const grid = document.createElement('div');
+    grid.className = 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8';
+
+    sortedRepos.forEach(repo => {
+      // Create repository card elements
+      const card = document.createElement('div');
+      card.className = 'course-card';
+
+      const image = document.createElement('img');
+      image.src = 'https://via.placeholder.com/500x300?text=' + repo.name;
+      image.alt = repo.name;
+
+      const title = document.createElement('h3');
+      title.className = 'course-title';
+      title.textContent = repo.name;
+
+      const description = document.createElement('p');
+      description.className = 'course-description';
+      description.textContent = repo.description;
+
+      // Append elements to repository card
+      card.appendChild(image);
+      card.appendChild(title);
+      card.appendChild(description);
+
+      // Append repository card to grid
+      grid.appendChild(card);
+    });
+
+    // Clear previous content and append the grid to the container
+    repoListElement.innerHTML = '';
+    repoListElement.appendChild(grid);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
